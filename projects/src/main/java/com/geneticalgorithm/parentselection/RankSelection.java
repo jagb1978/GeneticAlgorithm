4cadc7946e21b4984e0dd345e098b80567f533fb
Jose Alfredo Gonzalez <jgonzalez@appliedfinancialtechnology.com>
2017-10-05 17:55:18 +0100

    wip

--- projects/src/main/java/com/geneticalgorithm/parentselection/RankSelection.java
+++ projects/src/main/java/com/geneticalgorithm/parentselection/RankSelection.java
@@ -22,6 +22,6 @@ public class RankSelection {
 //            tournament.getIndividuals()[i] = population.getIndividuals()[randomId];
 //        }
 //        return tournament.getFittest();
-    }
+  //  }
 
 }
